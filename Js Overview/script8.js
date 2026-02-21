ec0330b378f6c6dbdf0af199d7a341e8f942ece5
shrenik <shrenik@gmail.com>
2026-02-21 14:00:32 +0530

    Read me updated

--- Js Overview/script8.js
+++ Js Overview/script8.js
@@ -13,9 +13,21 @@ let user = {
 }
 
 
-//Get the following format of the output
+/* Get the following format of the output
 output = {
     "Ashish":{rating:145},
     "Varun":{rating:189},
     "Vinod":{rating:144}
+}           */
+
+//user.profile.map(function(item, index){})
+let x = user.profile.map(item=>item.name); // ["Ashish", "Varun", "Vinod"]
+let y = user.rating.map(item=>item.count);
+output = {}
+
+for(let i=0;i<x.length;i++)
+{
+    output[x[i]]={rating:y[i]}
 }
+
+console.log(output);
